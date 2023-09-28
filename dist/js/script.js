c4e620d7b2785c3ac017d8439a639d0144c682a3
Evkron <KEV69R@yandex.ru>
2023-09-28 20:30:01 +0300

    Добавил скрипт  js  hiking  менялись  по клику

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,3 +15,36 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 });
+
+
+
+// ****************************************hiking************************************************
+const itemClicl = document.querySelector('.hiking__about_item_img');
+const img1Element = document.querySelector('.img1');
+const img2Element = document.querySelector('.img2');
+
+ itemClicl.onclick = imgToogle; 
+
+ function imgToogle () {
+   const img1 = img1Element.getAttribute('src') === 'img/about_hiking/1.png';
+   const img2 = img2Element.getAttribute('src') === 'img/about_hiking/2.png';
+
+  
+    if(img1) {
+        img1Element.src = 'img/about_hiking/2.png';
+        img1Element.setAttribute("alt", 'snowy mountains');
+    } else {
+        img1Element.src = 'img/about_hiking/1.png';
+    }   img1Element.setAttribute("alt", 'house and mountains in the background') 
+    if (img2) {
+        img2Element.src = 'img/about_hiking/1.png';
+        img1Element.setAttribute("alt", 'snowy mountains'); 
+    } else {
+        img2Element.src = 'img/about_hiking/2.png';
+        img1Element.setAttribute("alt", 'house and mountains in the background'); 
+        
+    }
+    // return imgToogle;
+   };
+
+// *******************************************************************************************
